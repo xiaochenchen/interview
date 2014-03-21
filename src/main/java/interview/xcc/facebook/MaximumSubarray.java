dddc9cbf340fde199930ad510aab04c0b6532dcb
Xiaochen Chen <xiaochenchen.git@gmail.com>
2014-03-20 23:28:42 -0400

    add maximum subarray as a facebook interview question

---
+++ src/main/java/interview/xcc/facebook/MaximumSubarray.java
@@ -0,0 +1,13 @@
+package interview.xcc.facebook;
+
+/**
+ * same as the leetcode Maximum Subarray
+ * asked during a facebook interview
+ * will be implemented in leetcode project
+ *
+ * Created by Maximus on 3/20/14.
+ */
+public class MaximumSubarray
+{
+
+}
